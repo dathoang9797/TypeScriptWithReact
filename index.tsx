3f085471b89c03ded2aee5358c71ebbbac2aa694
hoangquydat <46701740+dathoang9797@users.noreply.github.com>
2022-02-23 04:45:20 +0700

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,62 @@
+  {/* <Person name={personName} />
+      <PersonLists names={nameList} />
+      <Status status='error' />
+      <Oscar>
+        <Heading>Placeding hold text</Heading>
+      </Oscar>
+      <Greet name='Dat' isLoggedIn={true} />
+      <Button handleClick={(event, id) => {
+        console.log('Button CLick' + id);
+      }} />
+      <Input value='' handleChange={((e) => {
+        console.log(e);
+      })} /> */}
+      {/* <Container styles={{ border: '3px solid black', padding: '1rem' }} /> */}
+      {/* <LoggedIn /> */}
+      {/* <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider> */}
+
+      {/* <UserContextProvider>
+        <User />
+      </UserContextProvider> */}
+      {/* <DomRef />
+      <MutableRef /> */}
+      {/* <Counter message="The counter value is" /> */}
+      {/* <DemoMiniRedux /> */}
+      {/* <Private isLoggedIn component={Profile} /> */}
+      {/* <List items={['Batman', 'Superman', 'Wonder Woman']} onClick={(item) => {
+        console.log(item);
+      }} />
+      <List items={[1, 2, 3]} onClick={(item) => {
+        console.log(item);
+      }} /> */}
+      {/* <List items={
+        [
+          {
+            id: 1,
+            first: "Burce",
+            last: "Wayne"
+          },
+          {
+            id: 2,
+            first: "Clark",
+            last: "Kent"
+          },
+          {
+            id: 3,
+            first: "Princess",
+            last: "Diana"
+          }
+        ]
+      } onClick={(item) => {
+        console.log(item);
+      }} /> */}
+      {/* <RandomNumber value={10}   isZero isNegative /> */}
+      {/* <Toast position='center-left' /> */}
+
+      {/* <CustomButton variant='primary' onClick={() => { console.log('Click'); }}> Primary Button  </CustomButton> */}
+
+      <Text as='h1'  size='lg'>Heading</Text>
+      <Text as='p' size='md'>Paragraph</Text>
+      <Text as='label' htmlFor='someId' size='sm' color="secondary">Label</Text>
